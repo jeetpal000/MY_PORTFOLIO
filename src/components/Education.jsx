c7e68d670e2c02a51a7430981a9acdc0c7582d36
JEET <chandrajeetcare2019@gmail.com>
2026-01-22 21:21:43 +0530

    some changes on sm device

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -10,7 +10,7 @@ import "aos/dist/aos.css";
 const Education = () => {
   return (
     <>
-      <div data-aos="slide-right" className="flex items-center justify-center py-5 max-sm:py-2 max-sm:absolute max-sm:mt-30 max-sm:left-1/2 transform -translate-x-1/2 ">
+      <div data-aos="slide-right" className="flex items-center justify-center py-5 max-sm:py-2 max-sm:absolute max-sm:mt-22 max-sm:left-1/2  max-sm:transform max-sm:-translate-x-1/2 ">
         <span className="w-24 h-0.5 bg-[#1a1443]"></span>
         <span className="bg-[#1a1443] w-fit p-2 px-5 text-2xl rounded-md drop-shadow-[0_0_20px_#1e1463]">
           Education
